25be3be479c2a7b552e4df14e9acb22e84197ab2
Adel Faruque <Adelfaruque1@gmail.com>
2025-04-29 04:26:07 -0400

    removed selectability and draggability from all navbar elements. beginning Hero phase

--- website-framework/components/Navbar.tsx
+++ website-framework/components/Navbar.tsx
@@ -6,18 +6,18 @@ import ToggleMenu from "./ToggleMenu"
 const Navbar = () => {
 
   return (
-  <nav className="z-30 py-7 px-3 flex relative justify-end pr-10 items-center">
+  <nav className="z-30 py-7 px-3 flex relative justify-end pr-10 items-center select-none">
     
-    <Link href="/adel_resume.pdf" className="cursor-pointer left-10 absolute active:brightness-50">
+    <Link href="/adel_resume.pdf" className="cursor-pointer left-10 absolute active:brightness-50 select-none" draggable={false}>
       <div className="text-3xl text-white border-2 border-white rounded-lg py-2 px-5 ml-0 hover:bg-indigo-900">
-        <Image src={"/resume.svg"} alt="resume icon" width={45} height={45} className="inline-block pr-1"/>
+        <Image src={"/resume.svg"} alt="resume icon" width={45} height={45} className="inline-block pr-1" draggable={false}/>
         <span className="align-middle">Resume</span>
       </div>
     </Link>
 
     <ul className="hidden h-full gap-16 lg:flex flex-row"> 
       {NAV_LINKS.map((link) => (
-        <Link href={link.href} key={link.key} className="cursor-pointer text-xl hover:font-bold  text-white active:text-indigo-400">
+        <Link href={link.href} key={link.key} className="cursor-pointer text-xl hover:font-bold  text-white active:text-indigo-400" draggable={false}>
           //{link.label}
         </Link>
       ))}
--- website-framework/src/app/page.tsx
+++ website-framework/src/app/page.tsx
@@ -1,85 +1,12 @@
 import Image from "next/image";
 import Spacefiller from "../../components/Spacefiller";
+import Hero from "../../components/Hero";
 
 export default function Home() {
   return (
     <>
-      <div>
-        Hello World  
-      </div>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
-      <Spacefiller/>
+    
+      <Hero/>
       
     </>
   );
