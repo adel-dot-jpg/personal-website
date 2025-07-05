d29ea57c424bf245590b910325e9e7ff3ebcf636
Adel Faruque <Adelfaruque1@gmail.com>
2025-07-05 11:50:29 -0400

    idk, did something to navbar without saving so i'm committing again

--- website-framework/components/Navbar.tsx
+++ website-framework/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
   <nav className="z-30 py-7 px-3 flex relative justify-end pr-10 items-center select-none text-white">
     <div className="flex gap-3 lg:gap-10 absolute left-0 left-5 lg:left-10 items-center">
-      <Link href="/adel_resume.pdf" className="cursor-pointer  active:brightness-75 select-none border-0 border-blue-500" draggable={false}>
+      <Link href="/adel_resume.pdf" className="cursor-pointer active:brightness-75 select-none border-0 border-blue-500" draggable={false}>
         <div className="lg:text-2xl border-1 border-[var(--secondary)] rounded-lg py-2 px-5 ml-0 hover:bg-[var(--secondary)] transition-colors duration-300 text-gray-300">
           <Image src={"/resume.svg"} alt="resume icon" width={30} height={30} className="inline-block mr-1 -ml-1 -mt-0.5" draggable={false}/>
           <span className="align-middle">Resume</span>
@@ -26,9 +26,7 @@ const Navbar = () => {
         </TransitionLink>
       ))}
     </ul>
-    <div className="border-1 border-blue-500">
     <ToggleMenu/>
-    </div>
 
   </nav>
   )
