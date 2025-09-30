aa440029818b180fadc53b5b5ffa7be92fc4f915
Adel Faruque <Adelfaruque1@gmail.com>
2025-09-29 23:35:18 -0400

    cum. update: september 29th 2025

--- website-framework/components/Projects.tsx
+++ website-framework/components/Projects.tsx
@@ -1,30 +1,35 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import FakeTerminal from './FakeTerminal'
 import { PROJECTS } from '../../constants'
 
 const Projects = () => {
+  const [shuffled, setShuffled] = useState<typeof PROJECTS>([])
+
+  useEffect(() => { // prevent SSR mismatch
+    const shuffledProjects = [...PROJECTS]
+      .sort(() => Math.random() - 0.5)
+      .slice(0, 3)
+    setShuffled(shuffledProjects)
+  }, [])
+
   return (
-	< section id="projects">
-		<h2>
-			A tasting of my works
-		</h2>
-		<div className='flex justify-around flex-col xl:flex-row bg-[radial-gradient(closest-side_at_50%_50%,_var(--radial),_transparent)] border-0 border-red-500 overflow-auto gap-10 px-10'>
-			{[...PROJECTS] // copy so we don't mutate original
-			.sort(() => Math.random() - 0.5) // "shuffle"
-			.slice(0, 3) // pick first 3 projects after shuffle
-			.map((proj) => (
-				<FakeTerminal
-					title={proj.title}
-					key={proj.key}
-					description={proj.description}
-					technologies={proj.technologies}
-					status={proj.status}
-					updated={proj.lastUpdate}
-					href={proj.href}
-				/>
-			))}
-		</div>
-	</section>
+    <section id="projects">
+      <h2>A tasting of my work</h2>
+      <div className="flex justify-around flex-col xl:flex-row bg-[radial-gradient(closest-side_at_50%_50%,_var(--radial),_transparent)] border-0 border-red-500 overflow-auto gap-10 px-10">
+        {shuffled.map((proj) => (
+          <FakeTerminal
+            title={proj.title}
+            key={proj.key}
+            description={proj.description}
+            technologies={proj.technologies}
+            status={proj.status}
+            updated={proj.lastUpdate}
+            href={proj.href}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
